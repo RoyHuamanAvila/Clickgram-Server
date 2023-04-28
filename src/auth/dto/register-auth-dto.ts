61bd99e3cc12e131d55a6dc741fa778eb2d03623
Roy Huaman <RHavila789@gmail.com>
2023-04-27 22:57:33 -0500

    feat: login and register, auth jwt

---
+++ src/auth/dto/register-auth-dto.ts
@@ -0,0 +1,11 @@
+import { PartialType } from '@nestjs/swagger';
+import { LoginAuthDto } from './login-auth-dto';
+import { IsNotEmpty } from 'class-validator';
+
+export class RegisterAuthDto extends PartialType(LoginAuthDto) {
+  @IsNotEmpty()
+  username: string;
+
+  @IsNotEmpty()
+  fullname: string;
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,10 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User, UserDocument } from './user.schema';
 import { Model } from 'mongoose';
+import { RegisterAuthDto } from 'src/auth/dto/register-auth-dto';
 
 @Injectable()
 export class UserService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async createUser(userObject: RegisterAuthDto) {
+    const newUser = await new this.userModel(userObject);
+    return newUser.save();
+  }
 
   async getUsers() {
     const foundUsers = await this.userModel.find({});
